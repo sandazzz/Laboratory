21b3f5b31023e4c87c36289fc0a1acf5f56a4a01
sandazzz <rsandarisoa@gmail.com>
2025-12-09 16:40:05 +0100

    feat: implement stock map + getQuantity (GREEN)

--- src/main/java/com/sandazzz/laboratory/Laboratory.java
+++ src/main/java/com/sandazzz/laboratory/Laboratory.java
@@ -1,9 +1,18 @@
 package com.sandazzz.laboratory;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Laboratory {
+    private final Map<String, Double>stock = new HashMap<>();
+
     public Laboratory(List<String> substances) {
-        // Pour l'instant, aucune logique, juste assez pour que ça compile.
+        for (String s : substances) {
+            stock.put(s, 0.0);
+        }
     }
+     public double getQuantity(String substance) {
+        return stock.get(substance);
+     }
 }
