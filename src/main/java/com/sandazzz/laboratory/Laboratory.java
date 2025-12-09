abfff9538f132994286a986394d4799aea570ca9
sandazzz <rsandarisoa@gmail.com>
2025-12-09 16:33:12 +0100

    feat: minimal Laboratory constructor to satisfy test (GREEN)

--- src/main/java/com/sandazzz/laboratory/Laboratory.java
+++ src/main/java/com/sandazzz/laboratory/Laboratory.java
@@ -3,5 +3,7 @@ package com.sandazzz.laboratory;
 import java.util.List;
 
 public class Laboratory {
-
+    public Laboratory(List<String> substances) {
+        // Pour l'instant, aucune logique, juste assez pour que ça compile.
+    }
 }
